cd6c0ab39626a1b413ed962665b5c1d4751e58fd
youngx99 <1286739715@qq.com>
2025-05-11 11:32:25 +0800

    fix: 无用逻辑

--- src/components/toc.tsx
+++ src/components/toc.tsx
@@ -6,7 +6,6 @@ import * as React from "react"
 
 import { TableOfContents } from "@/lib/toc"
 import { cn } from "@/lib/utils"
-import { useMounted } from "@/hooks/use-mounted"
 
 interface TocProps {
   toc: TableOfContents
@@ -25,7 +24,7 @@ export function DashboardTableOfContents({ toc }: TocProps) {
     [toc]
   )
   const activeHeading = useActiveItem(itemIds)
-  const mounted = useMounted()
+  
 
   if (!toc?.items?.length) {
     return null
